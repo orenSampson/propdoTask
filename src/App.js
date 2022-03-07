7d77d6d5c525e50609aed67b5f39d271f44d3c9b
oren sampson <oren.sampson@gmail.com>
2022-03-07 21:36:26 +0200

    adding not found route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import NotFound from "./pages/NotFound/NotFound";
 import RealEstate from "./pages/RealEstate/RealEstate";
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Switch>
-        <Route exact path="/real-estate" component={RealEstate} />
+        <Route path="/real-estate" exact>
+          <RealEstate />
+        </Route>
+        <Route path="*">
+          <NotFound />
+        </Route>
       </Switch>
-    </BrowserRouter>
+    </Router>
   );
 }
 
